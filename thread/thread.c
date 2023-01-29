910fa478526b21c958a399c005db58895f8a0328
gming <a1804117268@gmail.com>
2023-01-29 15:48:04 +0800

    add syscall

--- thread/thread.c
+++ thread/thread.c
@@ -1,123 +1,102 @@
 #include "thread.h"
-#include "global.h"
+#include "stdint.h"
 #include "string.h"
-#include "memory.h"
-#include "list.h"
-#include "interrupt.h"
+#include "global.h"
 #include "debug.h"
+#include "interrupt.h"
+#include "print.h"
+#include "memory.h"
 #include "process.h"
+#include "sync.h"
 
-#define PG_SIZE 4096
+struct task_struct* main_thread;    // 主线程PCB
+struct list thread_ready_list;	    // 就绪队列
+struct list thread_all_list;	    // 所有任务队列
+struct lock pid_lock;		    // 分配pid锁
+static struct list_elem* thread_tag;// 用于保存队列中的线程结点
 
-task_struct* main_thread;
-list thread_ready_list;         //就绪队列
-list thread_all_list;           //所有任务队列
-static list_elem* thread_tag;  //保存队列中的线程节点
+extern void switch_to(struct task_struct* cur, struct task_struct* next);
 
-extern void switch_to(task_struct* cur, task_struct* next);
-
-/* 获取当前线程pcb的指针 */
-task_struct* running_thread() {
-    uint32_t esp;
-    asm volatile ("mov %%esp, %0" : "=g" (esp));
-    /* 取esp整数部分,即pcb起始地址 */
-    return (task_struct*)(esp & 0xfffff000);
+/* 获取当前线程pcb指针 */
+struct task_struct* running_thread() {
+   uint32_t esp; 
+   asm ("mov %%esp, %0" : "=g" (esp));
+  /* 取esp整数部分即pcb起始地址 */
+   return (struct task_struct*)(esp & 0xfffff000);
 }
 
-/* 由kernel_thread执行function(func_arg) */
+/* 由kernel_thread去执行function(func_arg) */
 static void kernel_thread(thread_func* function, void* func_arg) {
 /* 执行function前要开中断,避免后面的时钟中断被屏蔽,而无法调度其它线程 */
-    intr_enable();
-    function(func_arg);
+   intr_enable();
+   function(func_arg); 
 }
 
-/* 初始化线程thread_stack
-    将待执行的函数和参数放到thread_stack相应的位置 */
-void thread_create(task_struct* pthread, thread_func function, void* func_arg) {
-    //预留中断使用栈的空间
-    pthread->self_kstack -= sizeof(intr_stack);
-
-    //留出线程栈空间
-    pthread->self_kstack -= sizeof(thread_stack);
-    thread_stack* kthread_stack = (thread_stack*)pthread->self_kstack;
-    kthread_stack->eip = kernel_thread;
-    kthread_stack->function = function;
-    kthread_stack->func_arg = func_arg;
-    kthread_stack->ebp = kthread_stack->ebx = \
-    kthread_stack->esi = kthread_stack->edi = 0;
+/* 分配pid */
+static pid_t allocate_pid(void) {
+   static pid_t next_pid = 0;
+   lock_acquire(&pid_lock);
+   next_pid++;
+   lock_release(&pid_lock);
+   return next_pid;
 }
 
-void init_thread(task_struct* pthread, char* name, int prio) {
-    memset(pthread, 0, sizeof(*pthread));
-    strcpy(pthread->name, name);
+/* 初始化线程栈thread_stack,将待执行的函数和参数放到thread_stack中相应的位置 */
+void thread_create(struct task_struct* pthread, thread_func function, void* func_arg) {
+   /* 先预留中断使用栈的空间,可见thread.h中定义的结构 */
+   pthread->self_kstack -= sizeof(struct intr_stack);
 
-    if(pthread == main_thread) {
-        pthread->status = TASK_RUNNING;
-    } else {
-        pthread->status = TASK_READY;
-    }
-    //self_kstack是线程自己在内核态使用的栈顶地址
-    pthread->self_kstack = (uint32_t*)((uint32_t)pthread + PG_SIZE);
-    pthread->priority = prio;
-    pthread->ticks = prio;
-    pthread->elapsed_ticks = 0;
-    pthread->pgdir = NULL;
-    pthread->stack_magic = 0x19870916;  //自定义魔数
+   /* 再留出线程栈空间,可见thread.h中定义 */
+   pthread->self_kstack -= sizeof(struct thread_stack);
+   struct thread_stack* kthread_stack = (struct thread_stack*)pthread->self_kstack;
+   kthread_stack->eip = kernel_thread;
+   kthread_stack->function = function;
+   kthread_stack->func_arg = func_arg;
+   kthread_stack->ebp = kthread_stack->ebx = kthread_stack->esi = kthread_stack->edi = 0;
 }
 
-task_struct* thread_start(char* name, int prio,thread_func function, void* func_arg) {
-    //包括用户进程在内的pcb都位于内核空间
-    task_struct* thread = get_kernel_pages(1);
+/* 初始化线程基本信息 */
+void init_thread(struct task_struct* pthread, char* name, int prio) {
+   memset(pthread, 0, sizeof(*pthread));
+   pthread->pid = allocate_pid();
+   strcpy(pthread->name, name);
 
-    init_thread(thread, name, prio);
-    thread_create(thread, function, func_arg);
+   if (pthread == main_thread) {
+/* 由于把main函数也封装成一个线程,并且它一直是运行的,故将其直接设为TASK_RUNNING */
+      pthread->status = TASK_RUNNING;
+   } else {
+      pthread->status = TASK_READY;
+   }
 
-    //确保之前不在队列中
-    ASSERT(!elem_find(&thread_ready_list, &thread->general_tag));
-    /* 加入就绪队列 */
-    list_append(&thread_ready_list, &thread->general_tag);
-
-    //确保之前不在队列中
-    ASSERT(!elem_find(&thread_all_list, &thread->all_list_tag));
-    /* 加入全部线程队列 */
-    list_append(&thread_all_list, &thread->all_list_tag);
-
-    // asm volatile ("mov %0, %%esp; \
-    //                 pop %%ebp; pop %%ebx; pop %%edi; pop %%esi; \
-    //                 ": : "g" (thread->self_kstack) : "memory");
-    return thread;
+/* self_kstack是线程自己在内核态下使用的栈顶地址 */
+   pthread->self_kstack = (uint32_t*)((uint32_t)pthread + PG_SIZE);
+   pthread->priority = prio;
+   pthread->ticks = prio;
+   pthread->elapsed_ticks = 0;
+   pthread->pgdir = NULL;
+   pthread->stack_magic = 0x19870916;	  // 自定义的魔数
 }
 
-// 任务调度
-void schedule() {
-    ASSERT(intr_get_status() == INTR_OFF);
+/* 创建一优先级为prio的线程,线程名为name,线程所执行的函数是function(func_arg) */
+struct task_struct* thread_start(char* name, int prio, thread_func function, void* func_arg) {
+/* pcb都位于内核空间,包括用户进程的pcb也是在内核空间 */
+   struct task_struct* thread = get_kernel_pages(1);
 
-    task_struct* cur = running_thread(); // 获取当前正在执行的线程的 PCB 地址
+   init_thread(thread, name, prio);
+   thread_create(thread, function, func_arg);
 
-    if(cur -> status == TASK_RUNNING) { // 判断当前线程的时间片是否到期
-        ASSERT(!elem_find(&thread_ready_list, &cur -> general_tag));
-        // 那么就加入就绪队列
-        list_append(&thread_ready_list, &cur -> general_tag);
-        cur -> ticks = cur -> priority; // 重置时间片
-        cur -> status = TASK_READY;     // 重置状态为就绪状态
-    } else {
-        /* 若此线程需要某事件发生后才能继续上cpu运行,
-           不需要将其加入队列,因为当前线程不在就绪队列中。
-        */
-    }
+   /* 确保之前不在队列中 */
+   ASSERT(!elem_find(&thread_ready_list, &thread->general_tag));
+   /* 加入就绪线程队列 */
+   list_append(&thread_ready_list, &thread->general_tag);
 
-    ASSERT(!list_empty(&thread_ready_list));
-    thread_tag = NULL;
-    // 弹出队列的首元素，准备将其调度上处理器上执行
-    thread_tag = list_pop(&thread_ready_list);
-    task_struct* next = elem2entry(struct task_struct, general_tag, thread_tag);
-    next -> status = TASK_RUNNING;
-       /* ji活任务页表等 */
-    process_activate(next);
+   /* 确保之前不在队列中 */
+   ASSERT(!elem_find(&thread_all_list, &thread->all_list_tag));
+   /* 加入全部线程队列 */
+   list_append(&thread_all_list, &thread->all_list_tag);
 
-    switch_to(cur, next);
+   return thread;
 }
-
 
 /* 将kernel中的main函数完善为主线程 */
 static void make_main_thread(void) {
@@ -130,6 +109,35 @@ static void make_main_thread(void) {
  * 所以只将其加在thread_all_list中. */
    ASSERT(!elem_find(&thread_all_list, &main_thread->all_list_tag));
    list_append(&thread_all_list, &main_thread->all_list_tag);
+}
+
+/* 实现任务调度 */
+void schedule() {
+
+   ASSERT(intr_get_status() == INTR_OFF);
+
+   struct task_struct* cur = running_thread(); 
+   if (cur->status == TASK_RUNNING) { // 若此线程只是cpu时间片到了,将其加入到就绪队列尾
+      ASSERT(!elem_find(&thread_ready_list, &cur->general_tag));
+      list_append(&thread_ready_list, &cur->general_tag);
+      cur->ticks = cur->priority;     // 重新将当前线程的ticks再重置为其priority;
+      cur->status = TASK_READY;
+   } else { 
+      /* 若此线程需要某事件发生后才能继续上cpu运行,
+      不需要将其加入队列,因为当前线程不在就绪队列中。*/
+   }
+
+   ASSERT(!list_empty(&thread_ready_list));
+   thread_tag = NULL;	  // thread_tag清空
+/* 将thread_ready_list队列中的第一个就绪线程弹出,准备将其调度上cpu. */
+   thread_tag = list_pop(&thread_ready_list);   
+   struct task_struct* next = elem2entry(struct task_struct, general_tag, thread_tag);
+   next->status = TASK_RUNNING;
+
+   /* 击活任务页表等 */
+   process_activate(next);
+
+   switch_to(cur, next);
 }
 
 /* 当前线程将自己阻塞,标志其状态为stat. */
@@ -164,6 +172,7 @@ void thread_init(void) {
    put_str("thread_init start\n");
    list_init(&thread_ready_list);
    list_init(&thread_all_list);
+   lock_init(&pid_lock);
 /* 将当前main函数创建为线程 */
    make_main_thread();
    put_str("thread_init done\n");
--- thread/thread.h
+++ thread/thread.h
@@ -5,31 +5,32 @@
 #include "bitmap.h"
 #include "memory.h"
 
-/* 自定义通用函数类型, 在很多线程函数中作为形参类型 */
+/* 自定义通用函数类型,它将在很多线程函数中做为形参类型 */
 typedef void thread_func(void*);
+typedef int16_t pid_t;
 
 /* 进程或线程的状态 */
 enum task_status {
-    TASK_RUNNING,
-    TASK_READY,
-    TASK_BLOCKED,
-    TASK_WAITING,
-    TASK_HANGING,
-    TASK_DIED,
+   TASK_RUNNING,
+   TASK_READY,
+   TASK_BLOCKED,
+   TASK_WAITING,
+   TASK_HANGING,
+   TASK_DIED
 };
 
-/***************       中断栈intr_stack     ****************
- * 此结构用于中断发生时保存进程/线程的上下文环境
- * intr_exit的出栈操作是此结构的逆向操作
+/***********   中断栈intr_stack   ***********
+ * 此结构用于中断发生时保护程序(线程或进程)的上下文环境:
+ * 进程或线程被外部中断或软中断打断时,会按照此结构压入上下文
+ * 寄存器,  intr_exit中的出栈操作是此结构的逆操作
  * 此栈在线程自己的内核栈中位置固定,所在页的最顶端
- *********************************************************/
-typedef struct intr_stack {
-    uint32_t vec_no;        //kernel.S宏 宏VECTOR中push %1压入的中断号
+********************************************/
+struct intr_stack {
+    uint32_t vec_no;	 // kernel.S 宏VECTOR中push %1压入的中断号
     uint32_t edi;
     uint32_t esi;
     uint32_t ebp;
-    uint32_t esp_dummy;
-    //虽然pushad把esp压入，但是esp在不断变化，所以会被popad忽略
+    uint32_t esp_dummy;	 // 虽然pushad把esp也压入,但esp是不断变化的,所以会被popad忽略
     uint32_t ebx;
     uint32_t edx;
     uint32_t ecx;
@@ -39,71 +40,75 @@ typedef struct intr_stack {
     uint32_t es;
     uint32_t ds;
 
-    /* 以下由cpu从低特权级进入高特权级时压入 */
-    uint32_t error_code;        //error_code会在eip之后被压入
+/* 以下由cpu从低特权级进入高特权级时压入 */
+    uint32_t err_code;		 // err_code会被压入在eip之后
     void (*eip) (void);
     uint32_t cs;
     uint32_t eflags;
     void* esp;
     uint32_t ss;
-}intr_stack;
+};
 
-/********************   线程栈thread_stack  ******************
- * 线程自己的栈，用户存储线程中待执行的函数
- * 此结构在线程自己的内核栈中不固定
- * 仅用在switch_to时博爱村线程环境
- * 实际位置取决于实际情况
- ************************************************************/
-typedef struct thread_stack {
+/***********  线程栈thread_stack  ***********
+ * 线程自己的栈,用于存储线程中待执行的函数
+ * 此结构在线程自己的内核栈中位置不固定,
+ * 用在switch_to时保存线程环境。
+ * 实际位置取决于实际运行情况。
+ ******************************************/
+struct thread_stack {
    uint32_t ebp;
    uint32_t ebx;
    uint32_t edi;
    uint32_t esi;
 
-   /* 线程第一次执行时，eip指向待调用的函数kernel_thread
-    * 其他时候,eip是指向switch_to的返回地址 */ 
+/* 线程第一次执行时,eip指向待调用的函数kernel_thread 
+其它时候,eip是指向switch_to的返回地址*/
    void (*eip) (thread_func* func, void* func_arg);
 
-   /***** 以下仅供第一次被cpu调度时使用 *****/
+/*****   以下仅供第一次被调度上cpu时使用   ****/
 
-   /* 参数unused_ret只为占位置充数为返回地址 */
-   void (*unused_ret_addr);
-   thread_func* function;
-   void* func_arg;
-}thread_stack;
+/* 参数unused_ret只为占位置充数为返回地址 */
+   void (*unused_retaddr);
+   thread_func* function;   // 由Kernel_thread所调用的函数名
+   void* func_arg;    // 由Kernel_thread所调用的函数所需的参数
+};
 
+/* 进程或线程的pcb,程序控制块 */
 typedef struct task_struct {
-    uint32_t* self_kstack;      //各内核都用自己的内核栈
-    enum task_status status;
-    uint8_t priority;
-    char name[16];
-    uint8_t ticks;
+   uint32_t* self_kstack;	 // 各内核线程都用自己的内核栈
+   pid_t pid;
+   enum task_status status;
+   char name[16];
+   uint8_t priority;
+   uint8_t ticks;	   // 每次在处理器上执行的时间嘀嗒数
 
-    //此任务自运行起，占用了多少cpu滴答数
-    uint32_t elapsed_ticks;
+/* 此任务自上cpu运行后至今占用了多少cpu嘀嗒数,
+ * 也就是此任务执行了多久*/
+   uint32_t elapsed_ticks;
 
-    //general_tag用于线程在一般队列中的节点
-    list_elem general_tag;
+/* general_tag的作用是用于线程在一般的队列中的结点 */
+   struct list_elem general_tag;				    
 
-    //用于thread_list_all中的结点
-    list_elem all_list_tag;
+/* all_list_tag的作用是用于线程队列thread_all_list中的结点 */
+   struct list_elem all_list_tag;
 
-    uint32_t pgdir;             //进程自己虚拟页表的地址
-    struct virtual_addr userprog_vaddr;   // 用户进程的虚拟地址
-    uint32_t stack_magic;       //栈的边界标记，用于检测栈的溢出
+   uint32_t* pgdir;              // 进程自己页表的虚拟地址
+   struct virtual_addr userprog_vaddr;   // 用户进程的虚拟地址
+   struct mem_block_desc u_block_desc[DESC_CNT];   // 用户进程内存块描述符
+
+   uint32_t stack_magic;	 // 用这串数字做栈的边界标记,用于检测栈的溢出
 }task_struct;
+
 
 extern struct list thread_ready_list;
 extern struct list thread_all_list;
 
-task_struct* running_thread(void);
+void thread_create(struct task_struct* pthread, thread_func function, void* func_arg);
 void init_thread(struct task_struct* pthread, char* name, int prio);
+struct task_struct* thread_start(char* name, int prio, thread_func function, void* func_arg);
+struct task_struct* running_thread(void);
 void schedule(void);
-
+void thread_init(void);
 void thread_block(enum task_status stat);
 void thread_unblock(struct task_struct* pthread);
-void thread_init(void);
-void thread_create(task_struct* pthread, thread_func function, void* func_arg);
-task_struct* thread_start(char* name, int prio, thread_func function, void* func_arg);
-
 #endif
